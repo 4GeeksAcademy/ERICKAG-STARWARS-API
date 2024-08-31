a528df89bcf28cc02e8051a042fdad540f9e9c6a
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-08-31 02:16:23 +0000

    creando database

--- src/models.py
+++ src/models.py
@@ -17,3 +17,91 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    favoritos = db.relationship("Favoritos",backref="usuario",lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(250), nullable=False)
+    height= db.Column(db.String(250), nullable=False)
+    gender= db.Column(db.String(250), nullable=False)
+    favoritos = db.relationship("Favoritos",backref="character",lazy=True)
+        
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(250), nullable=False)
+    climate= db.Column(db.String(10), nullable=False)
+    terrain= db.Column(db.String(10), nullable=False)
+    favoritos = db.relationship("Favoritos",backref="planet",lazy=True)
+        
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(250), nullable=False)
+    model= db.Column(db.String(10), nullable=False)
+    class_name=db.Column(db.String(10), nullable=False)
+    favoritos = db.relationship("Favoritos",backref="vehicle",lazy=True)
+        
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer,db.ForeignKey('usuario.id'),nullable=False)
+    character_id= db.Column(db.Integer,db.ForeignKey('character.id'),nullable=True)
+    planet_id= db.Column(db.Integer, db.ForeignKey('planet.id'),nullable=True)
+    vehicle_id= db.Column(db.Integer,db.ForeignKey('vehicle.id'),nullable=True)
+        
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario": self.usuario_id,
+            "character": self.name,
+            "planet": self.character_id,
+            # do not serialize the password, its a security breach
+        }
